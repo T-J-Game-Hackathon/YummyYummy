27bd73ec85828ac64f534929388952f5ba67e70d
haniwa-820 <mt.fall820@gmail.com>
2022-12-21 17:54:53 +0900

    CameraContorollerの作成
    タイトル画面でフィールドが回転するように調整
    フィールドが完成した場合、MainCamera→CameraContoroller→TargetObjectの部分を変更する

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    [SerializeField] private float height;
+    [SerializeField] public float speed;
+    [SerializeField] public GameObject TargetObject;
+
+    void Start()
+    {
+        Transform Camera = this.transform;
+
+        Vector3 pos = Camera.position;
+        pos.x = TargetObject.transform.position.x;
+        pos.z = TargetObject.transform.position.z - radius;
+        Vector3 rad = Camera.eulerAngles;
+        rad.x = Mathf.Atan(height / radius);
+        rad.y = 0f;
+        rad.z = 0f;
+        Camera.eulerAngles = rad;
+    }
+
+    void Update()
+    {
+        transform.RotateAround(TargetObject.transform.position, new Vector3(0, 1, 0), speed);
+    }
+}
