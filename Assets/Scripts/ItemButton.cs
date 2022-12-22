25d1e6e83188dded0e45b80fbd8fa3198efe7ba0
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-12-22 17:59:52 +0700

    feat: ✨ shopのUIを整形

--- Assets/Scripts/ItemButton.cs
+++ Assets/Scripts/ItemButton.cs
@@ -2,25 +2,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-
 public class ItemButton : MonoBehaviour
 {
     public int price = 500;
     public int money = 3000;
     private Button itemButton;
 
-    void Awake()
-    {
-        if(money < 0)
-        {
-            
-        }
-        else if(money < price)
-        
-
-    }
     private void OnMoneyChange(int money)
     {
-        itemButton.interactable = money >= price ;
+        itemButton.interactable = money >= price;
     }
 }
